68204beb1e4229ad90d85aa50cef61af42512610
sureshsvs84 <suresh.s@smnetserv.com>
2019-08-08 00:55:01 +0530

    new page added

--- react-materialstrap/src/views/Pages/index.js
+++ react-materialstrap/src/views/Pages/index.js
@@ -2,8 +2,7 @@ import Login from './Login';
 import Page404 from './Page404';
 import Page500 from './Page500';
 import Register from './Register';
-import BlankPage from './BlankPage';
 
 export {
-  Login, Page404, Page500, Register,BlankPage
+  Login, Page404, Page500, Register
 };
